2caea2559e51803601ab3e04725e928edcc3e918
Lukasz Debiec <lukasz.r.debiec@gmail.com>
2018-03-22 16:45:44 +0100

    Found it! Number Wizard project retrieved!

---
+++ NumberWizardUI/Assets/LevelManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+public void LoadLevel (string name){
+Debug.Log("level load requested"+name);
+Application.LoadLevel(name);
+}
+public void QuitRequest () {
+Debug.Log ("Leave the game");
+Application.OpenURL("http://google.com/");
+}
+public void ShowTheCodeUnity (){
+Debug.Log ("Show the code - Unity");
+Application.OpenURL ("http://dietetykgraviola.pl/kod_gry/kodgryuproszczony.html");
+}
+public void ShowTheCodeConsole (){
+Debug.Log ("Show the code - Console");
+Application.OpenURL ("http://dietetykgraviola.pl/kod_gry/index.html");
+}
+}
